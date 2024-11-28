a230e74f64fba10bcf189704a1d6e76a641766e9
Franks-labs <franka.bockmann@gmail.com>
2024-11-28 18:32:54 +0100

    54645644

--- stats_Franka.py
+++ stats_Franka.py
@@ -3,22 +3,34 @@ import pandas as pd
 
 # Data frame format we need 
 
+#Intialise before first input beginning of new round
+#Update on every single guess for current round
+
 games_data = pd.DataFrame({
     "City_Given-Target_City": ["Tokio-Berlin", "Game 2", "Game 3", "Game 4", "Game 5"],
     "Guesses": [3, 4, 6, 2, 8]
 })
 
-# Variables we need 
-average_guesses_current = games_data["Guesses"].mean()
-average_guesses_updated = games_data["Guesses"].mean()
-delta = average_guesses_current - average_guesses_updated
+# Variables we need
 
+#Update every guess
+average_guesses_current = games_data["Guesses"].mean()
+average_guesses_previous = games_data["Guesses"].mean()
+delta = average_guesses_current - average_guesses_previous 
+
+#update every time a round is complete 
 number_of_rounds = len(games_data["City_Given-Target_City"])
 
+#update on every guess
 number_of_non_capitals_current = 32
 number_of_non_capitals_previous = 12
 
+#update on every guess
+distance_target_named = CHATGPT 
+how_far_off = [200, 500, 1000, 2]
+average_far_off = mean (how_far_off)
 
+#Update upon completon on every round
 # Display the bar chart using st.bar_chart
 
 st.title("How did you do? Let's review your stats.")
@@ -32,6 +44,7 @@ st.bar_chart(
     use_container_width=True
 )
 
+#Update on every single round
 # Display averages, total rounds, accuracy
 
 col1, col2 = st.columns(2)
