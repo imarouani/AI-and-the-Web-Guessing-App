e182f860be02215f99a23ef93b851b20a02b08f7
Franks-labs <franka.bockmann@gmail.com>
2024-11-28 17:36:59 +0100

    Update

--- stats_Franka.py
+++ stats_Franka.py
@@ -1,39 +1,44 @@
 import streamlit as st
 import pandas as pd
 
-# Create sample DataFrame with game names and guesses
+# Data frame format we need 
 
 games_data = pd.DataFrame({
-    "Historical Figure": ["Game 1", "Game 2", "Game 3", "Game 4", "Game 5"],
+    "City_Given-Target_City": ["Tokio-Berlin", "Game 2", "Game 3", "Game 4", "Game 5"],
     "Guesses": [3, 4, 6, 2, 8]
 })
 
 # Variables we need 
 average_guesses_current = games_data["Guesses"].mean()
-average_guesses_updated = 3
+average_guesses_updated = games_data["Guesses"].mean()
 delta = average_guesses_current - average_guesses_updated
 
-number_of_rounds = len(games_data["Historical Figure"])
+number_of_rounds = len(games_data["City_Given-Target_City"])
+
+number_of_non_capitals_current = 32
+number_of_non_capitals_previous = 12
 
 
 # Display the bar chart using st.bar_chart
 
-st.title("How did you do?")
+st.title("How did you do? Let's review your stats.")
 
 st.bar_chart(
     data=games_data,
-    x="Historical Figure",
+    x="City_Given-Target_City",
     y="Guesses",
-    x_label="Historical Figure",
+    x_label="Cities",
     y_label="Number of Guesses",
     use_container_width=True
 )
 
 # Display averages, total rounds, accuracy
 
-col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 col1.metric(label="Average Guesses per Game", value=f"{average_guesses_updated:.2f}", delta=delta, delta_color="inverse")
 col2.metric(label="Total Rounds", value=number_of_rounds)
-col3.metric("Accuracy", "86%", "4%")
 
 
+col1, col2 = st.columns(2)
+col1.metric("On average, how far off were your guesses?", "8600km", "400km",delta_color="inverse")
+col2.metric("How many non-capitals did you name?", number_of_non_capitals_current, number_of_non_capitals_previous, delta_color = "inverse")
